5edeb1c180ea9cef2455d91afdd14102ec5d2bb9
Víctor Martínez <victor.martinez@tecnativa.com>
2026-05-26 11:03:41 +0200

    [ADD] route_planning_rma_sale_delivery: New module

---
+++ route_planning_rma_sale_delivery/models/rma.py
@@ -0,0 +1,22 @@
+# Copyright 2025 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class Rma(models.Model):
+    _inherit = "rma"
+
+    reception_carrier_delivery_type = fields.Selection(
+        related="reception_carrier_id.delivery_type",
+        string="Reception carrier delivery type",
+    )
+    carrier_id = fields.Many2one(
+        inverse="_inverse_carrier_id",
+    )
+    carrier_delivery_type = fields.Selection(
+        related="carrier_id.delivery_type", string="Carrier delivery type"
+    )
+
+    def _inverse_carrier_id(self):
+        self._set_delivery_location_dest()
